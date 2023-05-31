d371a1e425a89f7267cf87af082700cbc3900b4b
Trezy <t@trezy.com>
2023-05-31 18:15:28 -0500

    remove debugging

--- packages/api/src/routes/xrpc/app.bsky.feed.getFeedSkeleton.js
+++ packages/api/src/routes/xrpc/app.bsky.feed.getFeedSkeleton.js
@@ -41,12 +41,6 @@ export const route = new Route({
 			'limit',
 		]
 
-		console.log({
-			headers: context.headers,
-			query: context.query,
-			url: context.url,
-		})
-
 		/** @type {QueryParams} */
 		const {
 			cursor,
