f7b1597c61d72db0c740babce97f1dbe70efd0d6
Trezy <t@trezy.com>
2023-05-31 11:37:03 -0500

    add optout tags

--- packages/sieve/src/filterSkeets.js
+++ packages/sieve/src/filterSkeets.js
@@ -1,3 +1,19 @@
+/**
+ * Determines whether a skeet should be stored in the database or not.
+ *
+ * @param {object} skeet
+ * @returns {boolean} Whether to store the skeet.
+ */
 export function filterSkeet(skeet) {
-	return /games?\s?(?:dev|design)/giu.test(skeet.text)
+	const { text } = skeet
+
+	if (!/games?\s?(?:dev|design)/giu.test(text)) {
+		return false
+	}
+
+	if (/#(?:nofeed|nogamedev|idontwanttogetfired)/giu.test(text)) {
+		return false
+	}
+
+	return true
 }
