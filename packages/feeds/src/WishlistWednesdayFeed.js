47893390b7cb5d832482169ac7ce9d0badb82b5c
Trezy <t@trezy.com>
2023-07-05 10:17:18 -0500

    add the wishlist wednesday feed

---
+++ packages/feeds/src/WishlistWednesdayFeed.js
@@ -0,0 +1,96 @@
+// Module imports
+import {
+	database,
+	Feed,
+} from '@trezystudios/bsky-common'
+
+
+
+
+
+/**
+ * Handler for the game dev feed.
+ */
+class WishlistWednesdayFeedClass extends Feed {
+	/****************************************************************************\
+	 * Public instance methods
+	\****************************************************************************/
+
+	/**
+	 * Generates the feed response for the API.
+	 *
+	 * @param {string} [cursor] A cursor for pagination.
+	 * @param {number} [limit] The number of skeets per page. Min 1, max 100.
+	 * @returns {Promise<{
+	 * 	cursor: string,
+	 * 	feed: object[],
+	 * }>} The generated feed.
+	 */
+	async generateFeed(cursor, limit = 30) {
+		const result = {}
+
+		const { skeets } = await database.getFeed(this.rkey, {
+			cursor,
+			limit,
+		})
+
+		result.feed = skeets.map(skeet => ({ post: skeet.uri }))
+
+		if (skeets.length) {
+			const lastSkeet = skeets.at(-1)
+
+			if (lastSkeet) {
+				result.cursor = Buffer.from(lastSkeet.uri).toString('base64')
+			}
+		}
+
+		return result
+	}
+
+	/**
+	 * Tests a skeet to verify whether it's relevant for this feed.
+	 *
+	 * @param {{
+	 * 	text: string,
+	 * 	replyParent: string,
+	 * }} skeet The skeet to test.
+	 * @returns {boolean} Whether the skeet is relevant.
+	 */
+	testSkeet(skeet) {
+		if (/#nofeed/giu.test(skeet.text)) {
+			return false
+		}
+
+		return /wishlist\s?wednesday/giu.test(skeet.text)
+	}
+
+
+
+
+
+	/****************************************************************************\
+	 * Public instance getters/setters
+	\****************************************************************************/
+
+	/** @returns {string} The description of the feed. */
+	get description() {
+		return 'Every Wednesday, game devs the world over post links to their games\' store pages! Wishlisting is a great way to support the games you\'re excited about, so hit that wishlist button liberally!'
+	}
+
+	/** @returns {string} The display name of the feed. */
+	get name() {
+		return 'Wishlist Wednesday'
+	}
+
+	/** @returns {string} The DID of the feed's owner. */
+	get ownerDID() {
+		return 'did:plc:4jrld6fwpnwqehtce56qshzv'
+	}
+
+	/** @returns {string} The record key of the feed. */
+	get rkey() {
+		return 'wishlist-wednesday'
+	}
+}
+
+export const WishlistWednesdayFeed = new WishlistWednesdayFeedClass
--- packages/feeds/src/index.js
+++ packages/feeds/src/index.js
@@ -1,3 +1,4 @@
 export { GameDevFeed } from './GameDevFeed.js'
 export { GameNewsFeed } from './GameNewsFeed.js'
 export { ScreenshotSaturdayFeed } from './ScreenshotSaturdayFeed.js'
+export { WishlistWednesdayFeed } from './WishlistWednesdayFeed.js'
