fd89e773d6398922293faa61fabdde5aa47a8fae
Trezy <t@trezy.com>
2023-07-05 12:42:52 -0500

    adding some more great blocklists

--- packages/sieve/src/index.js
+++ packages/sieve/src/index.js
@@ -23,6 +23,17 @@ import { logger } from './logger.js'
 // Constants
 /** @type {{ [key: string]: string[] }} */
 const blockListsMap = {
+	'estrogenempress.gay': [
+		// The Shadow Realm
+		'at://did:plc:uraielgolztbeqrrv5c7qbce/app.bsky.graph.list/3jxrx4pgy6h2p',
+
+		// Transphobes Who Should Leave Dr. Han's Office, NOW
+		'at://did:plc:uraielgolztbeqrrv5c7qbce/app.bsky.graph.list/3jxlfb5rt3w2e',
+
+		// Guys Who Will Not See Valhalla
+		'at://did:plc:uraielgolztbeqrrv5c7qbce/app.bsky.graph.list/3jxkgdumfox23',
+	],
+
 	'trezy.studio': [
 		// Game Feed Bans
 		'at://did:plc:pwsrgzcv426k7viyjl3ljdvb/app.bsky.graph.list/3jzcrcrh5b52h',
