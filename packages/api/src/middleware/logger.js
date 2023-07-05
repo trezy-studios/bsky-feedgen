90db99cb1eb014c9bd31f1f8db9df79d83f93658
Trezy <t@trezy.com>
2023-07-05 00:55:20 -0500

    add response time logging

--- packages/api/src/middleware/logger.js
+++ packages/api/src/middleware/logger.js
@@ -12,6 +12,8 @@ import { logger } from '../helpers/logger.js'
  * @param {Function} next The function to be executed when this middleware is ready to run the next middleware.
  */
 export async function loggerMiddleware(context, next) {
+	const now = performance.now()
+
 	logger.debug({
 		message: 'Received request',
 		headers: context.headers,
@@ -21,6 +23,11 @@ export async function loggerMiddleware(context, next) {
 	})
 
 	await next()
+
+	logger.debug({
+		message: 'response time',
+		value: performance.now() - now,
+	})
 
 	logger.debug({
 		message: 'Sending response',
